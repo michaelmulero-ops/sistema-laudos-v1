b07a8fc7b17afe23142773a2340e9d459a257ebd
michaelmulero-ops <michael.mulero@gmail.com>
2026-05-02 11:02:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import streamlit as st
 import os
-import subprocess
-import sys
-
-# Tenta instalar o Groq automaticamente se ele não estiver no servidor
-try:
-    from groq import Groq
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "groq"])
-    from groq import Groq
-
 import time
 from datetime import datetime
 from PIL import Image, ImageDraw
+from groq import Groq
