8abe0eae38c8a0d9a36a87ca5a6f131d3af20fed
michaelmulero-ops <michael.mulero@gmail.com>
2026-05-03 05:49:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import streamlit as st
 from PIL import Image, ImageOps, ImageDraw
 
-# Função para o Zoom de Evidência (Estilo a análise técnica que fizemos)
-def processar_zoom_tecnico(imagem, x, y):
-    img = Image.open(imagem)
-    # Define a área de corte (200px ao redor do ponto)
-    caixa = (x-100, y-100, x+100, y+100)
-    zoom = img.crop(caixa).resize((400, 400))
-    # Adiciona a borda vermelha de alerta
-    zoom = ImageOps.expand(zoom, border=5, fill='red')
-    return zoom
+# Estilização da interface para o modo "Sem Filtro"
+st.set_page_config(page_title="Michael Mulero Inspeções Tech V1", layout="wide")
