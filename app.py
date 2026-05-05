f0c026b9f7bef387e69c550dc9bdda3a6582bd3e
michaelmulero-ops <michael.mulero@gmail.com>
2026-05-04 21:58:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Adicione isso no topo do arquivo
-import plotly.express as px # Para os mapas 2D/3D
-
-# Dentro da função de gerar parecer, precisamos adicionar a lógica de blindagem:
-def gerar_croqui_blindagem(dados_inspecao):
-    # Aqui o programa deve criar a camada de proteção perimetral
-    # e marcar as câmeras e o SPDA de 9 pontos
-    pass
+import pandas as pd
+import folium # Biblioteca para criar os mapas de blindagem
+from streamlit_folium import st_folium
